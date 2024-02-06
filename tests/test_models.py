0cc22628fd92c54547ea524dddec93f828a5fd6a
taceroc <taceroc@unal.edu.co>
2024-02-06 13:22:35 -0500

    add test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -60,3 +60,26 @@ def test_max_mag_strings():
     test_input_colname = "b"
     with pytest.raises(TypeError):
         error_expected = max_mag('string', test_input_colname)
+
+
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        7),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+    ])
+def test_max_mag(test_df, test_colname, expected):
+    """Test max function works for array of zeroes and positive integers."""
+    from lcanalyzer.models import max_mag
+    assert max_mag(test_df, test_colname) == expected
+
