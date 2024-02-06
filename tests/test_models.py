bcd07fb253edb849ca748100ed1ddd41d05d36dd
taceroc <taceroc@unal.edu.co>
2024-02-06 12:25:48 -0500

    Add test case for min and mean functions

--- tests/test_models.py
+++ tests/test_models.py
@@ -25,3 +25,38 @@ def test_max_mag_zeros():
     test_output = 0
 
     assert max_mag(test_input_df, test_input_colname) == test_output
+
+
+def test_mean_mag_integers():
+    # Test that mean mag function works for integers
+    from lcanalyzer.models import mean_mag
+
+    test_input_df = pd.DataFrame(data=[[-1, 5, 3], 
+                                       [7, 8, 9], 
+                                       [3, 4, 1]], columns=list("abc"))
+    test_input_colname = "a"
+    test_output = (-1 + 7 + 3) / 3
+
+    assert mean_mag(test_input_df, test_input_colname) == test_output
+
+
+def test_min_mag_integers():
+    # Test that min_mag function works for integers
+    from lcanalyzer.models import min_mag
+
+    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
+                                       [7, 8, 9], 
+                                       [3, 4, 1]], columns=list("abc"))
+    test_input_colname = "a"
+    test_output = 1
+
+    assert min_mag(test_input_df, test_input_colname) == test_output
+
+import pytest
+def test_max_mag_strings():
+    # Test for TypeError when passing a string
+    from lcanalyzer.models import max_mag
+
+    test_input_colname = "b"
+    with pytest.raises(TypeError):
+        error_expected = max_mag('string', test_input_colname)
